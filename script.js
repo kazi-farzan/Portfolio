ca50c35b43ad9d4dc0498c7deaf9d91c17a2d2d8
Bunny5942 <kazifarzan23@gmail.com>
2024-05-28 16:54:00 +0530

    "Portfolio page 1 skeleton"

---
+++ script.js
@@ -0,0 +1,8 @@
+var tl = gsap.timeline();
+tl.from(".aspects-nav", {
+  y: 5,
+  opacity: 0,
+  duration: 1,
+  delay: 0.3,
+  stagger: 0.5,
+});
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["../index.html", "../css/**/*.css", "../script.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
